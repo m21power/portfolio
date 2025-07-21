88e5c59f158b12916e705500143f6ca330693e1a
mesay <mesaylema21@gmail.com>
2025-07-21 14:50:10 +0300

    fix: icon issue

--- src/components/commands/Education.tsx
+++ src/components/commands/Education.tsx
@@ -9,68 +9,124 @@ import {
 } from "react-icons/fa";
 import { MdSchool } from "react-icons/md";
 
+// Map icon names to their components with type assertion
+const iconMap = {
+  FaUniversity: FaUniversity as React.FC<
+    {
+      size?: number | string;
+      className?: string;
+    } & React.SVGProps<SVGSVGElement>
+  >,
+  MdSchool: MdSchool as React.FC<
+    {
+      size?: number | string;
+      className?: string;
+    } & React.SVGProps<SVGSVGElement>
+  >,
+  FaGraduationCap: FaGraduationCap as React.FC<
+    {
+      size?: number | string;
+      className?: string;
+    } & React.SVGProps<SVGSVGElement>
+  >,
+  FaGoogle: FaGoogle as React.FC<
+    {
+      size?: number | string;
+      className?: string;
+    } & React.SVGProps<SVGSVGElement>
+  >,
+  FaCode: FaCode as React.FC<
+    {
+      size?: number | string;
+      className?: string;
+    } & React.SVGProps<SVGSVGElement>
+  >,
+  FaTrophy: FaTrophy as React.FC<
+    {
+      size?: number | string;
+      className?: string;
+    } & React.SVGProps<SVGSVGElement>
+  >,
+};
+
+// Define the type for the eduBg items
+interface EduBgItem {
+  title: string;
+  desc: string;
+  iconName: keyof typeof iconMap; // Restrict to keys of iconMap
+  details?: {
+    text: string;
+    iconName: keyof typeof iconMap; // Restrict to keys of iconMap for details
+  }[];
+}
+
 const Education: React.FC = () => {
   return (
     <Wrapper data-testid="education">
       <EduIntro>My Education Background</EduIntro>
-      {eduBg.map(({ title, desc, details, icon }) => (
-        <EduList key={title}>
-          <div className="title">
-            <span className="icon">{icon}</span>
-            {title}
-          </div>
-          <div className="desc">{desc}</div>
-          {details && <div className="details">{details}</div>}
-        </EduList>
-      ))}
+      {eduBg.map(({ title, desc, details, iconName }) => {
+        const Icon = iconMap[iconName]; // Get the icon component for title
+        return (
+          <EduList key={title}>
+            <div className="title">
+              <span className="icon">
+                <Icon size={18} /> {/* Render the title icon */}
+              </span>
+              {title}
+            </div>
+            <div className="desc">{desc}</div>
+            {details && (
+              <div className="details">
+                <ul className="details-list">
+                  {details.map(({ text, iconName: detailIconName }, index) => {
+                    const DetailIcon = iconMap[detailIconName]; // Get the icon component for details
+                    return (
+                      <li key={index}>
+                        <DetailIcon size={14} className="inline-icon" />
+                        <span>{text}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            )}
+          </EduList>
+        );
+      })}
     </Wrapper>
   );
 };
 
-const eduBg = [
+const eduBg: EduBgItem[] = [
   {
     title: "Bachelor of Science in Computer Science",
     desc: "Addis Ababa University | Sep 2022 - Jun 2026",
-    icon: <FaUniversity size={18} />,
-    details: (
-      <ul className="details-list">
-        <li>
-          <FaGraduationCap size={14} className="inline-icon" />
-          <span>
-            Relevant Coursework: Data Structures & Algorithms, Mobile
-            Application Development, Object-Oriented Programming, Database
-            Management Systems
-          </span>
-        </li>
-      </ul>
-    ),
+    iconName: "FaUniversity",
+    details: [
+      {
+        iconName: "FaGraduationCap",
+        text: "Relevant Coursework: Data Structures & Algorithms, Mobile Application Development, Object-Oriented Programming, Database Management Systems",
+      },
+    ],
   },
   {
     title: "Competitive Programming and Coding Academy",
     desc: "Africa To Silicon Valley (A2SV) | Feb 2024 - Oct 2024",
-    icon: <MdSchool size={20} />,
-    details: (
-      <ul className="details-list">
-        <li>
-          <FaGoogle size={14} className="inline-icon" />
-          <span>
-            Google-backed program with strong focus on data structures and
-            algorithms
-          </span>
-        </li>
-        <li>
-          <FaCode size={14} className="inline-icon" />
-          <span>Solved 850+ problems on LeetCode and Codeforces</span>
-        </li>
-        <li>
-          <FaTrophy size={14} className="inline-icon" />
-          <span>
-            A2SV has placed 80+ students at top tech companies (Google,
-            Bloomberg, Databricks, Amazon, LinkedIn, Square)
-          </span>
-        </li>
-      </ul>
-    ),
+    iconName: "MdSchool",
+    details: [
+      {
+        iconName: "FaGoogle",
+        text: "Google-backed program with strong focus on data structures and algorithms",
+      },
+      {
+        iconName: "FaCode",
+        text: "Solved 850+ problems on LeetCode and Codeforces",
+      },
+      {
+        iconName: "FaTrophy",
+        text: "A2SV has placed 80+ students at top tech companies (Google, Bloomberg, Databricks, Amazon, LinkedIn, Square)",
+      },
+    ],
   },
 ];
 
